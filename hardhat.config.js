13858069c35b2c5f694c41c933536323ea80534b
Henate <Henate@126.com>
2022-08-19 23:59:01 +0800

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-};
+    solidity: "0.8.9",
+    networks: {
+        rinkeby: {
+            url: process.env.REACT_APP_RINKEBY_RPC,
+            accounts: [process.env.PRIVATE_KEY],
+            chainId: 4,
+        },
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+}
